dd1266b8563cb39261a3489cd543e9d7bc4eb3f6
Dan Gizzi <gizzidan@me.com>
2022-09-16 12:28:57 -0400

    Starting to create hit component

--- web/src/components/diversity-tags.tsx
+++ web/src/components/diversity-tags.tsx
@@ -5,13 +5,13 @@ import {
   Badge
 } from '@chakra-ui/react'
 
-const DiversityTags = ({ node, label }: any) => {
+const DiversityTags = ({ node, label, diverseOwnership }: any) => {
 
   return (
     < Text fontSize="md" > {label}
       {
-        node.company.diverseOwnership
-          ? node.company.diverseOwnership.sort().map((ownership: string) => {
+        diverseOwnership
+          ? diverseOwnership.sort().map((ownership: string) => {
             const color = ownership == "Minority-Owned" ? "gray" : ownership == "Women-Owned" ? "pink" : ownership == "Veteran-Owned" ? "cyan" : "whiteAlpha"
             return (
               <Badge key={ownership.toString()} mb={1} ml={2} variant="solid" fontSize="0.7rem" colorScheme={color}>{ownership.slice(0, -6)}</Badge>
--- web/src/components/search.tsx
+++ web/src/components/search.tsx
@@ -1,42 +1,88 @@
 import {
-  Box,
-  Button,
-  ButtonGroup,
-  Heading,
   Link,
-  List,
-  ListItem,
-  Text
+  Avatar,
+  Button,
+  Box,
+  Badge,
+  Tag,
+  VStack,
+  Grid,
+  GridItem,
+  HStack,
+  Wrap,
+  Text,
+  Heading,
 } from '@chakra-ui/react'
+import {
+  InstantSearch,
+  Hits,
+  SearchBox,
+  Pagination,
+  Highlight,
+  ClearRefinements,
+  RefinementList,
+  Configure,
+} from 'react-instantsearch-dom';
 import React from 'react'
+import { Link as GatsbyLink, useStaticQuery, graphql } from 'gatsby'
 import algoliasearch from "algoliasearch/lite";
-import { InstantSearch, Highlight, SearchBox, connectHits } from 'react-instantsearch-dom'
+import TimeAgo from 'react-timeago'
+import DiversityTags from './diversity-tags';
 
 
-const Hits = connectHits(({ hits }) => (
-  <Box>
-    {hits.length ? (
-      <>
-        <Text>
-          These are the results of your search. The title and excerpt are
-          displayed, though your search may have been found in the content of
-          the post as well.
-        </Text>
-        {hits.map(hit => {
-          return (
-            <Box>
-              <Heading>
-                <Highlight attribute="position" hit={hit} tagName="strong" />
-              </Heading>
-            </Box>
-          )
-        })}
-      </>
-    )
-      : <Text>No results for your search.</Text>
-    }
-  </Box>
-))
+const Hit = (props: any) => {
+  const node = props.hit
+  const minSalary = "$" + node.minAnnualSalary / 1000 + "k"
+  const maxSalary = "$" + node.maxAnnualSalary / 1000 + "k"
+  const bgColor = node.highlight === true ?
+    "mantis.50" : "whiteAlpha.200"
+  const bgHover = node.highlight === true ?
+    "mantis.100" : "blackAlpha.100"
+  const border = node.highlight === true ?
+    "5px solid" : "5px solid"
+  const borderColor = node.highlight === true ?
+    "mantis.500" : "transparent"
+  const buttonVariant = node.highlight == true ? "black" : "outline"
+  const time = node.stickyLength > 0
+    ? <Badge colorScheme="mantis">Featured</Badge>
+    : <Text><TimeAgo date={node._createdAt} /></Text>
+  return (
+    < Grid
+
+      p={3}
+      my={3}
+      templateColumns='repeat(5, 1fr)'
+      width="100%"
+      alignItems="center"
+      bg={bgColor}
+      _hover={{
+        bg: bgHover
+      }}
+      borderLeft={border}
+      borderColor={borderColor}
+      key={node.id}
+    >
+      <GridItem zIndex="docked" height="100%" gridRow={1} colStart={1} colSpan={5}>
+        <Link as={GatsbyLink} to={`/${node.path}`}><Box height="100%" ></Box></Link>
+      </GridItem>
+      <GridItem colStart={1} gridRow={1} colSpan={2}>
+        <HStack spacing={5}>
+          {node.includeLogo ?
+            node.logo
+              ? <Avatar name={node.companyName} src={node.logo}></Avatar>
+              : <Avatar color="black" bg="gray.200" name={node.companyName}></Avatar>
+            : <Avatar color="black" bg="blackAlpha.300" name={node.companyName}></Avatar>
+          }
+          <VStack spacing={1} align="left">
+            <Heading variant="card" as="h3">{node.position}</Heading>
+            <DiversityTags label={node.companyName} node={node} diverseOwnership={node.diverseOwnership} />
+            <Text variant="mono" fontSize="xs">{minSalary} - {maxSalary}</Text>
+          </VStack>
+        </HStack>
+      </GridItem>
+    </Grid>
+  )
+}
 
 const Search = () => {
   const algolia = algoliasearch(
@@ -50,7 +96,7 @@ const Search = () => {
       searchClient={algolia}
     >
       <SearchBox />
-      <Hits />
+      <Hits hitComponent={Hit} />
     </InstantSearch>
   )
 }
