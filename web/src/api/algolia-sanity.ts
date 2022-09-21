745a975e8804623be82eec07dd61c0e69e15d3c0
Dan Gizzi <gizzidan@me.com>
2022-09-21 14:17:19 -0400

    algolia

--- web/src/api/algolia-sanity.ts
+++ web/src/api/algolia-sanity.ts
@@ -103,7 +103,7 @@ const handler = (req: GatsbyFunctionRequest, res: GatsbyFunctionResponse) => {
 		// implement any `publishedAt` datetime visibility rules or other custom
 		// visibility scheme you may be using.
 		(document: SanityDocumentStub) => {
-			if (numDaysBetween(document.createdAt, new Date()) > 60) {
+			if (numDaysBetween(document._createdAt, new Date()) > 60) {
 				return false;
 			}
 			return true;
