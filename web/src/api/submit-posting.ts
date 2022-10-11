f01c547814bfffaf356dc4e0bfa6a10d21d4660a
Dan Gizzi <gizzidan@me.com>
2022-10-11 12:29:41 -0400

    Work on submit-posting function

--- web/src/api/submit-posting.ts
+++ web/src/api/submit-posting.ts
@@ -1,7 +1,7 @@
 import { GatsbyFunctionRequest, GatsbyFunctionResponse } from "gatsby";
 import { sanity } from "./algolia-sanity";
 
-export default function handler(
+export default async function handler(
 	req: GatsbyFunctionRequest,
 	res: GatsbyFunctionResponse
 ) {
@@ -10,4 +10,23 @@ export default function handler(
 		res.json({ message: "Bad request" });
 		return;
 	}
+	const posting = {
+		_type: "jobPosting",
+		position: req.body.position,
+		description: req.body.description,
+		primarySkill: req.body.primarySkill,
+		location: req.body.location,
+		applicationLink: req.body.applicationLink,
+		email: req.body.email,
+		minAnnualSalary: req.body.minSalary,
+		maxAnnualSalary: req.body.maxSalary,
+		includeLogo: req.body.includeLogo,
+		stickyLength: req.body.stickyLength,
+		highlight: req.body.highlight,
+		coupon: req.body.couponCode,
+	};
+
+	await sanity.create(posting).then((res) => {
+		console.log(`Job Posting was created, document ID is ${res._id}`);
+	});
 }
